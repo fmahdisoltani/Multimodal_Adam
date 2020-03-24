de9b67c12580cc129802fd4cc0b615ec9aa7dd7f
Farzaneh <farzaneh.mahdisoltani@twentybn.com>
2020-03-23 21:10:27 -0400

    Change tiny arch for regression

--- examples/classification/models/tiny.py
+++ examples/classification/models/tiny.py
@@ -9,13 +9,13 @@ __all__ = ['mlp']
 class TINY(nn.Module):
     def __init__(self, num_classes=10):
         super().__init__()
-        n_hid = 2
+        n_hid = 1
         n_out = 2
         self.l1 = nn.Linear(1, n_hid, bias=False)
         # self.l2 = nn.Linear(n_hid, n_out, bias=False)
 
-        s1 = torch.nn.Parameter(torch.tensor([[-1., 1.]]).T)
-        s2 = torch.nn.Parameter(torch.tensor([[1.]]).T)
+        s1 = torch.nn.Parameter(torch.tensor([[1.]]).T)
+        # s2 = torch.nn.Parameter(torch.tensor([[1.]]).T)
 
         self.l1.weight = s1
         # self.l2.weight = s2
